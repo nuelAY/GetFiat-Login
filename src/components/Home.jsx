fea01e30166c37edeba605c4bc6494865835ccaf
Manuel <nuelay99@gmail.com>
2024-01-26 10:50:48 +0100

    another commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 
 function Home() {
     return (
@@ -6,13 +10,21 @@ function Home() {
         <div className='text-center bg-zinc-950 mb-10 pb-3'> 
             <h1 className='py-3 text-white text-5xl md:text-7xl font-bold'>GetFiat</h1>
             <h2 className='text-white font-semibold'>Get Local Cash with Crypto</h2>
-            <p className='font-light text-xs text-zinc-200'>Seamlessly get FIAT in your local currency in minutes</p>
-
-            <p className='border border-slate-400 text-slate-500 w-[7rem] mx-[10rem] sm:mx-[40rem] my-5 py-3'>Powered by:</p>
-            <div></div>
+                <p className='font-light text-xs text-zinc-200'>Seamlessly get FIAT in your local currency in minutes</p>
+                
+                <div className='grid mx-[8rem] sm:mx-[20rem] my-5 py-3'>
+                    <p className='border border-slate-400 text-slate-500 w-[10rem] py-3 sm:w-[20rem]'>Powered by:</p>
+                    <h1 className='text-slate-200 text-2xl font-extrabold'>Swyft</h1>
+                    <div className='flex mx-10 my-2'>
+                        <FaInstagram fontSize={30} className='bg-white mx-2'/>
+                        <FaTwitter fontSize={30} className='bg-white'/>
+                    </div>
+                    
+                </div>
+        
         </div>
 
-        <div className='bg-zinc-950 w-[23rem] mx-6'>
+        <div className='bg-zinc-950 w-[23rem] mx-4'>
             <div className='mx-6 pt-10'>
                 <h2 className='text-white font-bold align-middle'>Get Started</h2>
                 <p className='text-zinc-200 font-extralight text-sm'>Register with your email to get started</p>
@@ -24,9 +36,9 @@ function Home() {
                 </div>
             </div>
 
-            <button type="submit" className="w-[20rem] mx-6 sm:mx-[33rem] bg-white text-zinc-500 py-2 my-[2rem] mb-[10rem] rounded hover:bg-zinc-800 focus:outline-none focus:shadow-outline-blue">
+            <Link type="submit" to="/Transaction" className="w-[20rem] mx-6 sm:mx-[33rem] bg-white text-zinc-500 py-2 my-[2rem] mb-[10rem] rounded hover:bg-zinc-800 text-center focus:outline-none focus:shadow-outline-blue">
                 Continue
-            </button>
+            </Link>
         </div>
             
     </div>
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { FaInstagram, FaTwitter } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Transaction() {
   return (
-    <div>Transaction</div>
+    <div className='bg-zinc-950 h-full'>
+      <h1 className='py-3 text-white text-5xl md:text-7xl text-center font-bold'>GetFiat</h1>
+        <div className='bg-zinc-950 w-[23rem] mx-4'>
+            <div className='mx-6 pt-10'>
+                <p className='text-zinc-200 font-extralight text-small'>Get Fiat in minutes</p>
+                </div>
+            <div className="flex-col items-center mb-[-2rem]">
+              <label for="email" className="block text-gray-200 text-sm font-bold mb-2 sr-only">Email:</label>
+              <div className="mx-6 sm:mx-[33rem] my-3">
+            <input type="number" name=" number" placeholder="Amount to receive (NGN) 500.000" class="w-[20rem] p-2 pl-8  bg-zinc-900" />
+            
+              </div>
+              <div className="mx-6 sm:mx-[33rem] my-3">
+                <input type="number" name=" number" placeholder="Amount to send (BTC-BTC) 0.02472367234636424623465" class="w-[20rem] p-2 pl-8  bg-zinc-900"/>
+              </div>
+        </div>
+        <p className='text-zinc-200 font-extralight text-small mx-[5rem] my-12 mb-[-1rem]'>You Will Receive NGN56000</p>
+            <Link type="submit" to="/Verification" className="w-[20rem] mx-6 sm:mx-[33rem] bg-white text-zinc-500 py-2 my-[2rem] mb-[10rem] rounded hover:bg-zinc-800 text-center focus:outline-none focus:shadow-outline-blue">
+                Continue
+            </Link>
+      </div>
+      
+
+
+      <div className='bg-zinc-950'>
+        <h1 className='py-3 text-white md:text-7xl text-center underline'>Get Transaction history</h1>
+        <div className='mx-6 text-center'>
+          <p className='text-zinc-200 font-extralight text-small'>billclinton@gmail.com</p>
+        </div>
+
+        <div className='flex mx-[3rem] sm:mx-[20rem] my-5 py-3'>
+          <div className='flex mx-3 my-2'>
+            <FaInstagram fontSize={30} className='bg-white mx-2'/>
+            <FaTwitter fontSize={30} className='bg-white'/>
+          </div>
+          <p className='border border-slate-400 text-slate-500 w-[20rem] px-12 py-3 sm:w-[20rem] mx-2'>Powered by:</p>
+          <h1 className='text-slate-200 text-2xl font-extrabold py-1'>Swyft</h1>
+                    
+                    
+                </div>
+      </div>
+            
+    </div>
   )
 }
 
